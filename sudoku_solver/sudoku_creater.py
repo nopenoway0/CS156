0cd2bc23e12603c25abbad43af2e6897dd83c06e
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-03-19 23:29:42 -0700

    fully working sudoku generator

--- sudoku_solver/sudoku_creater.py
+++ sudoku_solver/sudoku_creater.py
@@ -1,5 +1,7 @@
 import random
 import os
+from copy import deepcopy
+import re
 class Sudoku:
 	def __init__(self):
 		self.puzzle = []
@@ -7,14 +9,21 @@ class Sudoku:
 			self.puzzle.append([])
 			for y in range(0, 9):
 				self.puzzle[x].append(-1)
-		self.visible_p = self.puzzle
+		self.generate()
 				
 	def __str__(self):
-		str_p = ""
+		str_p = "   "
 		tmp_c = ""
+		for x in range(0, 9):
+			str_p = str_p + str(x) + " "
+		str_p = str_p + "    " + "\n"  + "   "
+		for x in range(0, 9):
+			str_p = str_p + "_ "
+		str_p = str_p + "\n"
 		for x in range(0,9):
+			str_p = str_p + str(x) + "| "
 			for y in range(0, 9):
-				tmp_c = self.puzzle[x][y]
+				tmp_c = self.visible_p[x][y]
 				if(tmp_c < 0 or tmp_c >= 10):
 					str_p += "."
 				else:
@@ -88,6 +97,7 @@ class Sudoku:
 					break
 			for r in range(0,10):
 				used_num[r] = 0
+		self.visible_p = deepcopy(self.puzzle)
 
 	def verify(self):
 		used_num = [0] * 10
@@ -111,16 +121,89 @@ class Sudoku:
 				used_num[r] = 0
 		return True
 
+	def hide_solution(self):
+		obfus_place = 0
+		reserved_placed = [0] * 9
+		for x in range(0,9):
+			for y in range(0,3):
+				while(reserved_placed[obfus_place] == 1):
+					obfus_place = random.randrange(0, 9)
+				reserved_placed[obfus_place] = 1
+			for y in range(0,9):
+				if(reserved_placed[y] != 1):
+					self.visible_p[x][y] = -1
+			for r in range(0,9):
+				reserved_placed[r] = 0
+		self.reset_puzzle = deepcopy(self.visible_p)
 
+	def get_solution(self):
+		str_p = ""
+		tmp_c = ""
+		for x in range(0,9):
+			for y in range(0, 9):
+				tmp_c = self.puzzle[x][y]
+				if(tmp_c < 0 or tmp_c >= 10):
+					str_p += "."
+				else:
+					str_p += str(tmp_c)
+				str_p += " "
+			str_p += "\n"
+		return str_p
 
-test = Sudoku()
-test.generate()
-'''
+	def get_prompt(self):
+		return "Type the place of the number to change - e.g. 0 0 2.\nThis will change the number at 0 0 to 2, if possible\nUse 0 as the number entered to reset that space.\nEnter \"quit\" to exit\nEnter \"submit\" to reveal and check answer\nEnter Command: "
+
+	def execute(self, arg):
+		tokens = re.split(r"-|\s+", arg)
+		if(int(tokens[2]) == 0):
+			tokens[2] = -1
+		if(self.reset_puzzle[int(tokens[0])][int(tokens[1])] == -1):
+			self.visible_p[int(tokens[0])][int(tokens[1])] = int(tokens[2])
+
+	def submit(self):
+		correct = 0
+		pre_placed = 0
+		for x in range(0,9):
+			for y in range(0,9):
+				if(self.visible_p[x][y] == self.puzzle[x][y]):
+					correct = correct + 1
+				if(self.reset_puzzle[x][y] != -1):
+					pre_placed += 1
+		self.visible_p = self.puzzle
+		return (correct - pre_placed, (81 - pre_placed))
+
+puzzle = Sudoku()
+puzzle.generate()
+puzzle.hide_solution()
+submitted = False
+while(1):
+
+	if(os.name is "nt"):
+		os.system("cls")
+	else:
+		os.system("clear")
+	print(puzzle)
+	if(submitted == False):
+		command = raw_input(puzzle.get_prompt())
+		if(command == "quit"):
+			break
+		elif(command == "submit"):
+			results = puzzle.submit()
+			print("you got: " + str(results[0]) + " correct out of " + str(results[1]))
+			raw_input("press enter to see solution...")
+			submitted = True
+		else:
+			try:
+				puzzle.execute(command)
+				break
+			except:
+				pass
+	else:
+		raw_input("press any key to start a new game")
+		puzzle.generate()
+		puzzle.hide_solution()
+		submitted = False
 if(os.name is "nt"):
 	os.system("cls")
 else:
 	os.system("clear")
-'''
-print(test)
-
-print("Integrity: " + str(test.verify()))
